2760c180085d2f361f47af4bce0b491f72d949d4
kgreen14 <kgreen14@gmail.com>
2017-09-05 14:49:03 -0400

    added components and some styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import ArtistList from "./components/ArtistList";
+import Artist from "./components/Artist";
+import SignUp from "./components/SignUp";
+import "./App.css";
+import GlobalNav from "./components/GlobalNav";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div>
+          <GlobalNav />
+          <div>
+          <Route exact path="/" component={ArtistList} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route path="/artist/:id" component={Artist} />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        </div>
+      </Router>
     );
   }
 }
